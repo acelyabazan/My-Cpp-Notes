888b99765007829819f381cb402e1036205a94c4
Açelya Bazan <111985667+acelyabazan@users.noreply.github.com>
2023-02-18 20:37:16 +0300

    Add files via upload

---
+++ 1_Bölüm.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+
+// Türkçe gibi karakter kullanımı için kütüphane locale.h kütüphanesini çağırıyoruz:
+#include <locale.h>
+
+using namespace std;
+
+int main() {
+
+	// Türkçe karakter komutunu çağırıyoruz:
+	setlocale(LC_ALL, "Turkish");
+
+	cout << "Welcome to Upcomm" << endl;
+
+	return 0;
+
+}
