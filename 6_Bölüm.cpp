d3e6e865e85d47b451284be628dc003dfebdd32c
Açelya Bazan <111985667+acelyabazan@users.noreply.github.com>
2023-02-20 20:10:44 +0300

    Add files via upload

---
+++ 6_Bölüm.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <locale.h>
+
+using namespace std;
+
+int main() {
+
+	setlocale(LC_ALL, "Turkish");
+
+	int kenar;
+
+	cout << "Lütfen Kenar Uzunluğunu Giriniz:" << " ";
+	cin >> kenar;
+
+	int cevre = kenar * 4;
+	int alan = kenar * kenar;
+
+	cout << "Karenin Çevresi:" << " " << cevre << endl;
+	cout << "Karenin Alanı:" << " " << alan << endl;
+
+	return 0;
+}
