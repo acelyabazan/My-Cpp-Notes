bade54f144d02a7bb6827fe323ba4da00d6c2405
Açelya Bazan <111985667+acelyabazan@users.noreply.github.com>
2023-02-20 20:26:23 +0300

    Add files via upload

---
+++ 7_Bölüm.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <locale.h>
+
+using namespace std;
+
+int main() {
+
+	setlocale(LC_ALL, "Turkish");
+
+	float pi = 3.14;
+	float r;
+
+	cout << "Dairenin Yarı Çapını Giriniz:" << " ";
+	cin >> r;
+
+	float cevre = 2 * pi * r;
+	float alan = pi * (r * r);
+
+	cout << "Dairenin Çevresi:" << " " << cevre << endl;
+	cout << "Dairenin Alanı:" << " " << alan << endl;
+}
