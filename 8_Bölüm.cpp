3048ca50905c2e7c4d3d3ff9f613b7969998b107
Açelya Bazan <111985667+acelyabazan@users.noreply.github.com>
2023-02-21 10:47:07 +0300

    Add files via upload

---
+++ 8_Bölüm.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <locale.h>
+
+using namespace std;
+
+int main() {
+
+	setlocale(LC_ALL, "Turkish");
+
+	float kupKenar;
+
+	cout << "Küpün Bir Kenar Uzunluğunu Girin:" << " ";
+	cin >> kupKenar;
+
+	float yuzeyAlani = 6 * (kupKenar * kupKenar);
+	float hacim = kupKenar * kupKenar * kupKenar;
+
+	cout << "Küpün Yüzey Alanı:" << " " << yuzeyAlani << endl;
+	cout << "Küpün Hacimi:" << " " << hacim << endl;
+
+	return 0;
+}
