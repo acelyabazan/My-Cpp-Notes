870b7e51a23febadaf0801874bfc45b68ddb7fd0
Açelya Bazan <111985667+acelyabazan@users.noreply.github.com>
2023-02-21 19:16:00 +0300

    Add files via upload

---
+++ 13_Bölüm.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <locale.h>
+
+int main() {
+
+	setlocale(LC_ALL, "Turkish");
+
+	int sayi;
+
+	std::cout << "Lütfen bir sayı giriniz:" << " ";
+	std::cin >> sayi;
+
+	if (sayi > 0) {
+		std::cout << "Girmiş olduğunuz sayı:" << " " << sayi << " " << "pozitif bir sayıdır." << std::endl;
+	}
+	else if (sayi < 0) {
+		std::cout << "Girmiş olduğunuz sayı:" << " " << sayi << " " << "negatif bir sayıdır." << std::endl;
+	}
+	else {
+		std::cout << "Girmiş olduğunuz sayı:" << " " << sayi << std::endl;
+	}
+
+	return 0;
+}
