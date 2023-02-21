422acfa6cc061c9795eb71ec0a48d668ef4ab1dd
Açelya Bazan <111985667+acelyabazan@users.noreply.github.com>
2023-02-21 11:32:36 +0300

    Add files via upload

---
+++ 10_Bölüm.cpp
@@ -0,0 +1,29 @@
+﻿// 6 basamaklı bir sayının, her basamağındaki rakamı topluyoruz. Bu denklemi aşağıdaki gibi çözebilirsiniz.
+
+#include <iostream>
+#include <locale.h>
+
+using namespace std;
+
+int main() {
+	
+	setlocale(LC_ALL, "Turkish");
+
+	int altiBasamakliSayi;
+
+	cout << "6 Basamaklı Bir Sayı Giriniz:" << " ";
+	cin >> altiBasamakliSayi;
+
+	int yuzbinler = (altiBasamakliSayi / 100000);
+	int onbinler = (altiBasamakliSayi % 100000) / 10000;
+	int binler = (altiBasamakliSayi % 10000) / 1000;
+	int yuzler = (altiBasamakliSayi % 1000) / 100;
+	int onlar = (altiBasamakliSayi % 100) / 10;
+	int birler = (altiBasamakliSayi % 10);
+
+	int toplam = yuzbinler + onbinler + binler + yuzler + onlar + birler;
+
+	cout << altiBasamakliSayi << " " << "Sayının değerlerinin Toplamı: " << " " << toplam << endl;
+
+	return 0;
+}
