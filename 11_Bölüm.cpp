22c01f6e3dea8d412752975517214789afa4af37
Açelya Bazan <111985667+acelyabazan@users.noreply.github.com>
2023-02-21 12:12:35 +0300

    Add files via upload

---
+++ 11_Bölüm.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <locale.h>
+
+using namespace std;
+
+int main() {
+
+	setlocale(LC_ALL, "Turkish");
+
+	string isim = "Açi";
+	cout << isim << endl;
+
+
+
+	string ad, soyAd, yas;
+
+	cout << "Adınızı Giriniz:" << " " << ad;
+	cin >> ad;
+
+	cout << "Soy Adınızı Giriniz:" << " " << soyAd;
+	cin >> soyAd;
+
+	cout << "Yaşınızı Giriniz:" << " " << yas;
+	cin >> yas;
+
+	cout << "Adınız:" << " " << ad << endl;
+	cout << "Soy Adınız:" << " " << soyAd << endl;
+	cout << "Yaşınız:" << " " << yas << endl;
+
+	return 0;
+}
