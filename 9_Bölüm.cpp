aaeb8d7e55884f1efcb78b56ce9d83de7cc07ea5
Açelya Bazan <111985667+acelyabazan@users.noreply.github.com>
2023-02-21 11:02:18 +0300

    Add files via upload

---
+++ 9_Bölüm.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <locale.h>
+
+using namespace std;
+
+int main() {
+
+	setlocale(LC_ALL, "Turkish");
+
+	float a, b, c, x;
+
+	cout << "Lütfen 'a' değerini giriniz:" << " ";
+	cin >> a;
+
+	cout << "Lütfen 'b' değerini giriniz:" << " ";
+	cin >> b;
+
+	cout << "Lütfen 'c' değerini giriniz:" << " ";
+	cin >> c;
+
+	cout << "Lütfen 'x' değerini giriniz:" << " ";
+	cin >> x;
+
+	float denklem = (a * (x * x)) + (b * x) + c;
+
+	cout << "ax² + bx + c =" << " " << denklem << endl;
+
+	return 0;
+}
