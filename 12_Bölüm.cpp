94948e935517f898dae57f0d4f64e33aa8c63da1
Açelya Bazan <111985667+acelyabazan@users.noreply.github.com>
2023-02-21 17:59:38 +0300

    Add files via upload

---
+++ 12_Bölüm.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <locale.h>
+
+// using namespace std; yerine artık kütüphanelerin hepsinde kodun başına std:: kullanıyoruz.
+
+// stl için ayrı bir kaynak araştırmalısın Açi!
+
+int main() {
+
+	// Operatörler:
+	// == : eşit eşittir
+	// != : eşit eşit değildir
+	// <  : büyüktür
+	// >  : küçüktür
+	// <= : büyüktür veya eşittir
+	// >= : küçüktür veya eşittir
+	// && : ve 
+	// || : veya
+
+	// Bu operatörler sonucu ya True değeri verecek ya da False değerini bizlere verecektir.
+
+	setlocale(LC_ALL, "Turkish");
+
+	int sayi1;
+	int sayi2;
+
+	std::cout << "Birinci Sayıyı Giriniz:" << " ";
+	std::cin >> sayi1;
+
+	std::cout << "İkinci Sayıyı Giriniz:" << " ";
+	std::cin >> sayi2;
+
+	if (sayi1 < sayi2) {
+		std::cout << sayi1 << " " << "<" << " " << sayi2 << std::endl;
+	}else if (sayi1 > sayi2) {
+		std::cout << sayi1 << " " << ">" << " " << sayi2 << std::endl;
+	}else if (sayi1 == sayi2) {
+		std::cout << sayi1 << " " << "=" << " " << sayi2 << std::endl;
+	}
+	return 0;
+}
