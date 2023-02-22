f8fc18c6f6d71bfbfd444a33e9cb71ca1ec9412c
Açelya Bazan <111985667+acelyabazan@users.noreply.github.com>
2023-02-22 16:19:39 +0300

    Add files via upload

---
+++ 14_Bölüm.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <locale.h>
+
+
+int main() {
+
+	setlocale(LC_ALL, "Turkish");
+
+	int a;
+	int b;
+	int c;
+
+	std::cout << "Lütfen Üçgenin Birinci Kenarını Giriniz:" << " ";
+	std::cin >> a;
+
+	std::cout << "Lütfen Üçgenin İkinci Kenarını Giriniz:" << " ";
+	std::cin >> b;
+
+	std::cout << "Lütfen Üçgenin Üçüncü Kenarını Giriniz:" << " ";
+	std::cin >> c;
+
+	int toplam = a + b + c;
+
+	if (a > 0 && b > 0 && c > 0){
+
+	if (a == b && b == c){
+
+	std::cout << "Butun kenarlar esit oldugu icin Eskenar Ucgendir" << std::endl;
+	}
+	else if (a == b || b == c || a == c){
+
+	std::cout << "Sadece iki kenar esit oldugu icin Ikizkenar Ucgendir" << std::endl;
+	}
+	else
+	{
+	std::cout << "Kenarları esit olmadigi icin Cesitkenar Ucgendir" << std::endl;
+	}
+	}
+	else {
+		std::cout << "Lütfen tam sayı giriniz!" << std::endl;
+	}
+
+	return 0;
+}
