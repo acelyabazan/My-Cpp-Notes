f9d958e6ddde222b8fe3d294c7a441c7c9867e38
Açelya Bazan <111985667+acelyabazan@users.noreply.github.com>
2023-02-28 18:00:51 +0300

    Add files via upload

---
+++ 19_Bölüm.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+int main() {
+	setlocale(LC_ALL, "Turkish");
+
+	int sinir;
+	int toplam = 0;
+
+	std::cout << "Kaçtan başlayarak geriye yazdırmak istediğinizi giriniz: ";
+	std::cin >> sinir;
+
+	// İlk while döngümüz:
+	while (sinir > 0) {
+		std::cout << sinir << std::endl;
+
+		// Her while döngüsü döndüğünde, sinir değişkenimiz 1 azalacaktır:
+		toplam += sinir;
+		sinir--;
+	}
+
+	std::cout << "Toplam: " << toplam;
+
+	return 0;
+}
