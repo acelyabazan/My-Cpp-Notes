a2121644f71cfa5f08c3daaf0ac16ef7a62d935f
Açelya Bazan <111985667+acelyabazan@users.noreply.github.com>
2023-03-03 22:31:10 +0300

    Add files via upload

---
+++ 20_Bölüm.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <typeinfo>
+
+int main() {
+	setlocale(LC_ALL, "Turkish");
+
+	int tamSayi;
+	int toplam = 1;
+
+
+
+	std::cout << "Bir tam sayı giriniz: ";
+	std::cin >> tamSayi;
+
+	if (std::cin.fail() || std::cin.eof()){ // Kullanıcının girdiği değer tam sayı değilse:
+
+		std::cout << "Lütfen tam sayı giriniz!" << std::endl;
+	}
+	else {
+		while (tamSayi > 0) {
+
+			toplam *= tamSayi;
+			tamSayi--;
+		}
+		std::cout << toplam << std::endl;
+	}
+
+	return 0;
+}
+
+// Bu kodda, std::cin.fail() ve std::cin.eof() işlevlerini kullanarak girilen değerin tam sayı olup olmadığını kontrol ediyoruz. Eğer tam sayı değilse, hata mesajı veriyoruz; aksi takdirde faktöriyel hesaplanıyor ve sonuç ekrana yazdırılıyor.
