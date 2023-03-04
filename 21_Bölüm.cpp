4377cd511785671cfba009ebf85f97b31307bd09
Açelya Bazan <111985667+acelyabazan@users.noreply.github.com>
2023-03-04 12:36:17 +0300

    Add files via upload

---
+++ 21_Bölüm.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+
+int main() {
+	setlocale(LC_ALL, "Turkish");
+
+	// do while döngüsünün farkı ilk önce do'nun içerisini yapar ondan sonra while döngüsüne başlar.
+	// Örneğin:
+
+	int sayi;
+
+	std::cout << "Lütfen bir tam sayı giriniz: ";
+	std::cin >> sayi;
+
+	do {
+		std::cout << sayi << std::endl;
+		sayi--;
+
+	} while (sayi > 0); {
+
+	}
+
+	return 0;
+}
---
+++ 22_Bölüm.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+
+int main() {
+	setlocale(LC_ALL, "Turkish");
+
+	int n;
+	int tekSayilarinToplami = 0;
+	int ciftSayilarinToplami = 0;
+
+	std::cout << "Lütfen tam sayı giriniz: ";
+	std::cin >> n;
+
+	if (std::cin.fail() || std::cin.eof()) { // Kullanıcının girdiği değer tam sayı değilse:
+
+		std::cout << "Lütfen tam sayı giriniz!" << std::endl;
+
+	}
+	else {
+		do {
+			if (n % 2 == 0) {
+				std::cout << n << " Çift sayıdır." << std::endl;
+				tekSayilarinToplami += n;
+			}
+			else if (n % 2 == 1) {
+				std::cout << n << " Tek sayıdır." << std::endl;
+				ciftSayilarinToplami += n;
+			}
+			n--;
+		} while (n > 0); {
+			std::cout << "Tek sayıların toplamı: " << tekSayilarinToplami << std::endl;
+			std::cout << "Tek sayıların toplamı: " << ciftSayilarinToplami << std::endl;
+		}
+	}
+
+	return 0;
+}
