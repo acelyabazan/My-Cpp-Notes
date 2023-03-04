581377be266e386ea9f0427d22d9dc897614c6a9
Açelya Bazan <111985667+acelyabazan@users.noreply.github.com>
2023-03-04 19:23:46 +0300

    Add files via upload

---
+++ 23_Bölüm.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+
+int main() {
+	setlocale(LC_ALL, "Turkish");
+
+	// for döngüsünde ram üzerinde geçici değişken oluşturuyoruz, koşulunu belirliyoruz ve bundan sonra ne yapılması gerektiğini belirliyoruz:
+
+	int tekSayilarinToplami = 0;
+	int ciftSayilarinToplami = 0;
+
+	for (int i = 100; i > 0;) {
+
+		do {
+			if (i % 2 == 0) {
+				std::cout << i << " Çift sayıdır" << std::endl;
+				tekSayilarinToplami += i;
+			}
+			else if (i % 2 == 1) {
+				std::cout << i << " Tek sayıdır" << std::endl;
+				ciftSayilarinToplami += i;
+			}
+			i--;
+		} while (i > 0); {
+			std::cout << "Tek sayıların toplamı: " << tekSayilarinToplami << std::endl;
+			std::cout << "Çift sayıların toplamı: " << ciftSayilarinToplami << std::endl;
+		}
+	}
+
+	return 0;
+}
---
+++ 24_Bölüm.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+
+int main()
+{
+	setlocale(LC_ALL, "Turkish");
+
+	for (int carpan = 1; carpan < 11; carpan++) {
+		for (int carpilan = 1; carpilan < 11; carpilan++) {
+
+			int carpmaIslemi = carpan * carpilan;
+			std::cout << carpan << "x " << carpilan << " = " << carpmaIslemi << std::endl;
+		}
+	}
+
+	return 0;
+}
