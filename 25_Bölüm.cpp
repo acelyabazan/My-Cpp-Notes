7b9f909d5a5e0e1a23005b04516a241865d52bff
Açelya Bazan <111985667+acelyabazan@users.noreply.github.com>
2023-03-05 13:53:55 +0300

    Add files via upload

---
+++ 25_Bölüm.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+
+int main()
+{
+    setlocale(LC_ALL, "Turkish");
+
+    // break döngüyü kırmak, continue ise altındaki altındaki kodları çalıştırmak döngüye devam etmek demektir:
+
+    int i;
+    int toplam = 0;
+
+    std::cout << "Lütfen tam sayı giriniz: ";
+    std::cin >> i;
+
+    if (std::cin.fail() || std::cin.eof()) {
+        std::cout << "Lütfen tam sayı giriniz!";
+    }
+    else {
+        do {
+            std::cout << i << std::endl;
+            toplam += i;
+            i--;
+            continue;
+        } while (i > 0); {
+            std::cout << "Toplam: " << toplam << std::endl;
+        }
+    }
+
+    return 0;
+}
